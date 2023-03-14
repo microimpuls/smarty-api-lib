027cdadae69888a6beda964c25ead11c30293b84
superdef454 <superdef454@mail.ru>
2023-03-14 17:58:34 +0500

    content: python: #15397 поправил названия полей

--- content-api/python/import_video_from_file.py
+++ content-api/python/import_video_from_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import csv
-import openpyxl # pip install openpyxl, for parsing xlsx
+# for parsing xlsx
+import openpyxl # pip install openpyxl
 try:
     from client import SmartyContentAPI
 except ImportError:
@@ -29,14 +30,15 @@ class ImportVideoFromFile():
         with open(self.file_path, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                name = row.pop('Name')
-                rating = row.pop('Rating')
+                name = row.pop('name')
+                rating = row.pop('rating')
                 if not name or not rating:
                     print("Exception: not name or rating")
                     continue
                 for key, value in row.items():
                     kwargs[key] = value
-                print(self.api.video_create(name, rating, **kwargs))
+                self.api.video_create(name, rating, **kwargs)
+                kwargs.clear()
 
     def parse_xlsx(self):
         wookbook = openpyxl.load_workbook(self.file_path)
@@ -46,9 +48,9 @@ class ImportVideoFromFile():
             for col in worksheet.iter_cols(1, worksheet.max_column):
                 if col[i].value and col[0].value:
                     kwargs[col[0].value] = col[i].value
-            name = kwargs.pop('Name')
-            rating = kwargs.pop('Rating')
-            print(self.api.video_create(name, rating, **kwargs))
+            name = kwargs.pop('name')
+            rating = kwargs.pop('rating')
+            self.api.video_create(name, rating, **kwargs)
             kwargs.clear()
 
 if __name__ == "__main__":
