d743b752761f9d2d1b16bcd3699daa4dd572fe13
BergaBruh <bergirium@gmail.com>
2023-09-06 14:51:58 +0500

    update_films_data: Исправление ошибок
    Добавлены недостающие type hints. Добавлено описание для класса и функции. Переделаны некоторые моменты по поводу вывода ошибок.

--- content-api/python/update_films_data.py
+++ content-api/python/update_films_data.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from urllib.error import URLError, HTTPError
 import logging
 import datetime
 try:
-    from client import SmartyContentAPI, ContentAPIException
+    from client import SmartyContentAPI
 except ImportError:
     raise ImportError('client.py file not found')
 
@@ -14,50 +13,54 @@ content_api_key: str = 'api_key' # ContentAPI ключ. Нужен для обн
 client_id: int = 1
 
 
-class ParseError(Exception):
-    pass
-
-
 class UpdateVideoList(object):
-    def __init__(self, api, videos_per_page):
-        self.api = api
-        self.videos_per_page = videos_per_page
+    """Класс, представляющий собой обновление метаданных фильмов и сериалов в Smarty.
 
-    def get_pages(self, limit: int, count: int):
-        pages = count//limit
+    Note:
+        Для работоспособности скрипта требуется чтобы в Smarty у фильма/сериала было вбито в поле "ID фильма на кинопоиске" само ID поле.
+    """
+    def __init__(self, api: SmartyContentAPI):
+        self.api = api
+
+    def _get_pages(self, limit: int, count: int):
+        pages = count // limit
         if count % limit != 0:
             pages += 1
-        return pages
+        return pages+1
 
-    def update_video(self, video_files: int):
-        try:
-            api.video_modify(id=video_files, load_meta=1)
-        except URLError or HTTPError:
-            ParseError()
+    def _update_video(self, video_files: int):
+        api.video_modify(id=video_files, load_meta=1)
 
-    def update_content(self, videos):
+    def _update_content(self, videos):
         for video in videos:
             video_files = video['id']
             try:
-                self.update_video(video_files)
+                self._update_video(video_files)
                 print(f'Информация о фильме с ID = {video_files} обновлена успешно')
                 logging.info(f'Информация о фильме с ID = {video_files} обновлена успешно')
-            except ContentAPIException as error:
+            except Exception as error:
                 print(f'Произошла ошибка при обновлении информации медиатеки для фильма/сериала {video_files}.'
                       f' Для уточнения откройки последний лог-файл')
                 logging.error(f'Произошла ошибка при обновлении информации медиатеки для фильма/сериала {video_files}.'
                               f' Сообщение об ошибки: {repr(error)}')
-                break
+                continue
 
-    def load_data(self):
-        # videos_per_page = 5
-        response = self.api.video_list(page=1, limit=self.videos_per_page)
-        self.update_content(response['videos'])
-        if response['count'] > self.videos_per_page:
-            pages = self.get_pages(self.videos_per_page, response['count'])
-            for i in range(2, pages+1):
-                response = self.api.video_list(page=i, limit=self.videos_per_page)
-                self.update_content(response['videos'])
+    def run(self):
+        """Запуск скрипта обновления метаданных фильмов и сериалов в Smarty.
+
+        Args:
+            videos_per_page (:obj:`int`): Количество фильмов и сериалов на одну страницу.
+            response (:obj:`SmartyContentAPI`): Ответ, получаемый от Smarty. В данном случаи - список видео.
+            pages (:obj:`int`): Номер страницы.
+        """
+        videos_per_page: int = 1
+        response = self.api.video_list(page=1, limit=videos_per_page)
+        self._update_content(response['videos'])
+        if response['count'] > videos_per_page:
+            pages = self._get_pages(videos_per_page, response['count'])
+            for i in range(2, pages):
+                response = self.api.video_list(page=i, limit=videos_per_page)
+                self._update_content(response['videos'])
 
 
 if __name__ == '__main__':
@@ -65,5 +68,5 @@ if __name__ == '__main__':
                         filename=f"update_media_list_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
                         filemode="w", format="%(asctime)s %(levelname)s %(message)s")
     api = SmartyContentAPI(smarty_url, client_id, content_api_key)
-    update_videos = UpdateVideoList(api, videos_per_page=1) # Примечание: можно в videos_per_page указать количество фильмов на страницу от 1 до 100
-    update_videos.load_data()
+    update_videos = UpdateVideoList(api)
+    update_videos.run()
