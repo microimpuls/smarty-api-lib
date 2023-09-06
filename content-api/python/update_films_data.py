e1fe4a9e14c9254da4601d577ebefc73ca23f287
BergaBruh <bergirium@gmail.com>
2023-09-06 10:31:13 +0500

    update_films_data: переписывание кода
    Код был переписан по аналогии с sync_content

--- content-api/python/update_films_data.py
+++ content-api/python/update_films_data.py
@@ -1,50 +1,69 @@
-# -*- coding: utf-8 -*
-from client import SmartyContentAPI, ContentAPIException
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 from urllib.error import URLError, HTTPError
-
 import logging
 import datetime
-
-smarty_url = 'http://example.com/'  # Ссылка на Smarty
-client_id = 1  # ID клиента Smarty
-content_api_key = 'top secret'  # ContentAPI ключ. Нужно для обновления данных о фильмах
-
-
-def get_from_smarty_films_in_the_form_json_list(page):
-    limit_film = 100 # Количество фильмов/Сериалов на одну страницу
-    number = 0
-
-    get_api_response = api.video_list(limit=limit_film, page=page)
-    video_count = get_api_response['count'] # Получение сколько всего фильмов/сериалов
-    videos_info = get_api_response['videos'] # Получение данных о фильме
-    get_from_smarty_films_data(videos_info, video_count, number, page, limit_film)
+try:
+    from client import SmartyContentAPI, ContentAPIException
+except ImportError:
+    raise ImportError('client.py file not found')
 
 
-def get_from_smarty_films_data(videos_info, video_count, number, page, limit_film):
-    while video_count > page:
+smarty_url: str = 'http://127.0.0.1:8180/' # Ссылка на Smarty
+content_api_key: str = 'api_key' # ContentAPI ключ. Нужен для обновления
+client_id: int = 1
+
+
+class ParseError(Exception):
+    pass
+
+
+class UpdateVideoList(object):
+    def __init__(self, api, videos_per_page):
+        self.api = api
+        self.videos_per_page = videos_per_page
+
+    def get_pages(self, limit: int, count: int):
+        pages = count//limit
+        if count % limit != 0:
+            pages += 1
+        return pages
+
+    def update_video(self, video_files: int):
         try:
-            id_number = videos_info[number]['id'] # Получение ID фильма
+            api.video_modify(id=video_files, load_meta=1)
+        except URLError or HTTPError:
+            ParseError()
+
+    def update_content(self, videos):
+        for video in videos:
+            video_files = video['id']
             try:
-                api.video_modify(id=id_number, load_meta=1) # Обновление данных фильма (требуется вбитый kinopoisk_id в Smarty)
-                logging.info(
-                    f"Для фильма/сериала с ID {id_number} была обновлена информация через Kinopoisk Unofficial "
-                    f"API и добавлена в Smarty.")
-            except HTTPError or URLError or IndexError:
-                logging.error(f"Ошибка при обновлении данных о фильме {id_number}. Пропускаем...", exc_info=True)
-                pass
-            number += 1
-            if number == limit_film:
-                page += 1
-                get_from_smarty_films_in_the_form_json_list(page)
-        except IndexError or HTTPError or URLError or ContentAPIException:
-            logging.error('Незивеста ошибка! Ошибка: ', exc_info=True)
-            break
+                self.update_video(video_files)
+                print(f'Информация о фильме с ID = {video_files} обновлена успешно')
+                logging.info(f'Информация о фильме с ID = {video_files} обновлена успешно')
+            except ContentAPIException as error:
+                print(f'Произошла ошибка при обновлении информации медиатеки для фильма/сериала {video_files}.'
+                      f' Для уточнения откройки последний лог-файл')
+                logging.error(f'Произошла ошибка при обновлении информации медиатеки для фильма/сериала {video_files}.'
+                              f' Сообщение об ошибки: {repr(error)}')
+                break
+
+    def load_data(self):
+        # videos_per_page = 5
+        response = self.api.video_list(page=1, limit=self.videos_per_page)
+        self.update_content(response['videos'])
+        if response['count'] > self.videos_per_page:
+            pages = self.get_pages(self.videos_per_page, response['count'])
+            for i in range(2, pages+1):
+                response = self.api.video_list(page=i, limit=self.videos_per_page)
+                self.update_content(response['videos'])
 
 
 if __name__ == '__main__':
-    api = SmartyContentAPI(base_url=smarty_url, client_id=client_id, api_key=content_api_key)
     logging.basicConfig(level=logging.INFO,
-                        filename=f"SmartyKinopoisk_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
+                        filename=f"update_media_list_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
                         filemode="w", format="%(asctime)s %(levelname)s %(message)s")
-    get_from_smarty_films_in_the_form_json_list(page=1)
+    api = SmartyContentAPI(smarty_url, client_id, content_api_key)
+    update_videos = UpdateVideoList(api, videos_per_page=1) # Примечание: можно в videos_per_page указать количество фильмов на страницу от 1 до 100
+    update_videos.load_data()
